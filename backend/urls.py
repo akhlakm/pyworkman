48a29a5a61fe237848c12b6d8da37541344c347b
akhlakm <me@akhlakm.com>
2023-11-19 23:10:49 -0500

    Add main view

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .main import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', index, name='main'),
     path("", TemplateView.as_view(template_name='main.html')),
 ]
