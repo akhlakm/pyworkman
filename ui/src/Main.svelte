d7daaf7448edfd9a076c6a2295dcce93a9b225c7
akhlakm <me@akhlakm.com>
2023-11-23 13:29:00 -0500

    Show job list on empty definition

--- ui/src/Main.svelte
+++ ui/src/Main.svelte
@@ -4,7 +4,7 @@
     import JobStatus from "./lib/JobStatus.svelte";
     import ServiceList from "./lib/ServiceList.svelte";
 
-    import { alert, job_status, job_definition } from "./store";
+    import { alert, job_status, job_definition, job_list } from "./store";
     import { send } from "./utils";
     import { onMount } from "svelte";
     import JobInput from "./JobInput.svelte";
@@ -15,15 +15,16 @@
 </script>
 
 <TopBar />
+<ServiceList />
 {#if $alert}
     <h3 class="mx-auto text-red-500 w-full text-left px-10">{$alert}</h3>
 {/if}
 
 {#if $job_status}
     <JobStatus status={$job_status} />
-{:else if $job_definition}
-    <JobInput />
+{:else if $job_list}
+    {#if $job_definition}
+        <JobInput />
+    {/if}
     <ServiceDetails />
-{:else}
-    <ServiceList />
 {/if}
