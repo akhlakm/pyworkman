87500f5089514b99c8890748f401537f7b162ab9
akhlakm <me@akhlakm.com>
2023-11-23 23:22:55 -0500

    Reorganize server start functions

--- workman/__main__.py
+++ workman/__main__.py
@@ -1,10 +1,11 @@
-import os
-import django
-from django.core import management
+from workman import broker, backend
+from multiprocessing import Process
 
 def main():
-    print("Hello")
+    p1 = Process(target=broker.start)
+    p2 = Process(target=backend.start)
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workman.backend.settings')
-    django.setup()
-    management.call_command('runserver')
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
