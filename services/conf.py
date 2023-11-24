bcd51de6f64295fc1d98b09589a635c03e41fa1c
akhlakm <me@akhlakm.com>
2023-11-23 23:46:11 -0500

    Add postgres config

--- services/conf.py
+++ services/conf.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from workman.util.makeconf import Config, Bool
 
+
 @dataclass
 class _workman:
     """ WorkMan configurations """
@@ -8,8 +9,18 @@ class _workman:
     log_level : int = 8
 
 
-_c = Config("config.yaml").load_yaml()
+@dataclass
+class _postgres:
+    db_host : str = 'localhost'
+    db_port : int = 5432
+    db_user : str = 'postgres'
+    db_pswd : str = ''
+    db_name : str = 'postgres'
+
+
+_c = Config("svc-config.yaml").load_yaml()
 WorkMan     = _c.section(_workman)
+PostGres    = _c.section(_postgres)
 
 if __name__ == '__main__':
     _c.save_yaml()
