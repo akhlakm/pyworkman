0583210ef520691ec79506bf2577236076919a0b
akhlakm <me@akhlakm.com>
2023-11-23 22:34:49 -0500

    Run backend from the main script

--- workman/__main__.py
+++ workman/__main__.py
@@ -1,2 +1,10 @@
+import os
+import django
+from django.core import management
+
 def main():
     print("Hello")
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workman.backend.settings')
+    django.setup()
+    management.call_command('runserver')
