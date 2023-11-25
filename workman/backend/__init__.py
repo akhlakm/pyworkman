56729a9d300b002b9d80b1d2e466c11dcefef727
akhlakm <me@akhlakm.com>
2023-11-25 18:40:47 -0500

    Apply backend migrations

--- workman/backend/__init__.py
+++ workman/backend/__init__.py
@@ -6,4 +6,5 @@ def start():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workman.backend.settings')
     django.setup()
     management.call_command('collectstatic', '--noinput')
+    management.call_command('migrate')
     management.call_command('runserver')
