48c5ff133d35bd2be6241e2ea684d8034accdf2e
akhlakm <me@akhlakm.com>
2023-11-25 18:12:21 -0500

    Add copy command to copy an example

--- workman/__main__.py
+++ workman/__main__.py
@@ -3,7 +3,8 @@ from workman import conf
 
 def usage():
     print("USAGE:")
-    print("\tworkman mgr|ui|update")
+    print("\tworkman mgr|ui|update|copy")
+    print("\tworkman copy <example name>")
     sys.exit(1)
 
 
@@ -26,6 +27,16 @@ def main():
         # Update the python packages.
         from workman.util import shell
         shell.execute_command("pip install -U pyworkman", capture=False)
+
+    elif cmd == "copy":
+        # Copy an example to current directory.
+        import os, shutil
+        if len(sys.argv) < 3:
+            usage()
+        name = sys.argv[2]
+        shutil.copyfile(f"{os.path.dirname(__file__)}/examples/{name}.py",
+                        f"./{name}.py")
+        print("Copy OK:", name)
 
     else:
         usage()
