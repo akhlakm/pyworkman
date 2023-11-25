7b3058e36c4c73fa5cf068f7bb315310a682fe9f
akhlakm <me@akhlakm.com>
2023-11-24 19:53:01 -0500

    Save config on execution

--- services/conf.py
+++ services/conf.py
@@ -5,7 +5,7 @@ from workman.util.makeconf import Config, Bool
 @dataclass
 class _workman:
     """ WorkMan configurations """
-    mgr_url : str = 'tcp://127.0.0.1:5555'
+    mgr_url : str = 'tcp://127.0.0.1:5455'
     log_level : int = 8
 
 
@@ -27,3 +27,6 @@ _c = Config("config-svc.yaml").load_yaml()
 WorkMan     = _c.section(_workman)
 PostGres    = _c.section(_postgres)
 Workers     = _c.section(_workers)
+
+if __name__ == '__main__':
+    _c.save_yaml()
